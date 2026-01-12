97c11b5bbd0ed3e6a7e9d86b78b066a9d58e9520
honwenxuan_omesti <wx.hon@omesti.com>
2026-01-12 17:12:02 +0800

    Refactor DatabaseSeeder to use call method for seeding classes for easy seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            IngredientSeeder::class,
+            RecipeSeeder::class,
+            RecipeIngredientSeeder::class,
+            StepsSeeder::class,
+        ]);
     }
 }
